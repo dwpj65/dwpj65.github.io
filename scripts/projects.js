e3adbbc7632e0a7e54f6da9058ac0e91dea51543
dwpj65 <dwpj65@yahoo.com>
2020-09-25 13:21:16 -0400

    clock script running, using innerHTML

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,6 +1,13 @@
 // https://www.w3schools.com/js/js_whereto.asp
 
-// window.onload = function() {
-//     alert("Loaded");
-//     console.log('Loaded');
-// };
+window.onload = function() {
+    // alert("Loaded");
+    // console.log('Loaded');
+    updateTimeDisplay()
+};
+
+updateTimeDisplay = function() {
+    var date = new Date()
+    document.getElementById("clock").innerHTML = date
+    var t = setTimeout(function(){ updateTimeDisplay() }, 1000); /* setting timer */
+}
